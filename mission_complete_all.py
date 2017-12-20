7efd9002c9481d9bdec6b2392b189d5b2570d492
huangwenjie <i@cirfi.com>
2017-12-20 11:50:06 +0800

    跳过已完成的任务

--- mission_complete_all.py
+++ mission_complete_all.py
@@ -8,6 +8,8 @@ kirara_api.set_unity_user_agent("app/0.0.0; Android OS 4.4.2 / API-19 KOT49H/3.8
 kirara_api.load_account()
 kirara_api.login(kirara_api.user_account["uuid"],kirara_api.user_account["accesstoken"])
 
+bypass = []
+
 while True:
     r = kirara_api.mission_get()
     r.pop("resultCode")
@@ -20,12 +22,23 @@ while True:
         item["rate"] = item["rateMax"]
     t = kirara_api.mission_set(r)
     print(t)
+
+    if len(r["missionLogs"]) == len(bypass):
+      print("任务已全部完成")
+      break;
+
+    index = 0
     for item in r["missionLogs"]:
+        index += 1
+        if index in bypass:
+          continue
         mission_dict = {
             "managedMissionId": item["managedMissionId"]
         }
         try:
             t = kirara_api.mission_complete(mission_dict)
+            if t["resultCode"] == 510:
+              bypass.append(index)
             print(t)
         except:
             pass
