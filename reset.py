aa49736c29c17175f1329a32fb730a6a73c7ba17
llc1123 <i@acgpl.us>
2017-12-21 03:24:44 -0600

    土豆服务器应对

--- reset.py
+++ reset.py
@@ -4,42 +4,45 @@ api = libkirara.KiraraAPI()
 
 while True:
 
-	api.getVersion()
-	api.signUp()
-	api.login()
-	api.miscFirstLogin()
-	api.gachaLoop()
+	try:
+		api.getVersion()
+		api.signUp()
+		api.login()
+		api.miscFirstLogin()
+		api.gachaLoop()
 
-	daily = 20
-	other = 20
-	f5 = 20
+		daily = 20
+		other = 20
+		f5 = 20
 
-	api.mission_refresh()
-
-	for i in range(daily):
-		api.mission_completeDaily()
-		api.mission_completeWeekly()
-		print('Daily and weekly missions routine progress...' + str(i+1) + '/' + str(other))
-
-	for i in range(other):
 		api.mission_refresh()
-		api.mission_completeOther()
-		print('Other missions routine progress...' + str(i+1) + '/' + str(other))
 
-	api.fetchPresents()
+		for i in range(daily):
+			api.mission_completeDaily()
+			api.mission_completeWeekly()
+			print('Daily and weekly missions routine progress...' + str(i+1) + '/' + str(other))
 
-	for i in range(f5):
-		for j in range(5):
-			api.mission_completeF5()
-			print('F-5 missions routine progress...' + str(i*5+j+1) + '/' + str(f5*5))
+		for i in range(other):
+			api.mission_refresh()
+			api.mission_completeOther()
+			print('Other missions routine progress...' + str(i+1) + '/' + str(other))
+
 		api.fetchPresents()
 
-	print('Fucking Kirara for gems...Complete.')
+		for i in range(f5):
+			for j in range(5):
+				api.mission_completeF5()
+				print('F-5 missions routine progress...' + str(i*5+j+1) + '/' + str(f5*5))
+			api.fetchPresents()
 
-	api.gachaChrist()
+		print('Fucking Kirara for gems...Complete.')
 
-	api.dropAll()
+		api.gachaChrist()
 
-	f = open('output.txt', 'a')
-	f.write(api.uuid + ',' + api.accessToken + ',' + api.moveCode + ',1234\n')
-	f.close()
+		api.dropAll()
+
+		f = open('output.txt', 'a')
+		f.write(api.uuid + ',' + api.accessToken + ',' + api.moveCode + ',1234\n')
+		f.close()
+	except:
+		pass
